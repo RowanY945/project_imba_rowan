f1fab74345b9078a1e01542409ef7b2bcc552d4d
RowanY945 <ruiweny96@gmail.com>
2025-01-16 17:43:03 +1100

    Add files via upload

---
+++ IaC/SFN/terraform.tfvars
@@ -0,0 +1,2 @@
+aws_region     = "ap-southeast-2"
+sfn_policy_name = "lambda-invocation-role"
---
+++ IaC/SFN/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+  description = "AWS region for the resources"
+  type        = string
+}
+variable "sfn_policy_name" {
+  description = "Name of the SFN"
+  type        = string
+}
